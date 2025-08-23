ebf7e2a35409557a8dd44a79024ea2d6f3fc3561
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2025-08-23 21:09:42 +0530

    nodemailer config

---
+++ server/config/nodemailer.js
@@ -0,0 +1,12 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  auth: {
+    user: process.env.SMTP_USER,
+    PASS: process.env.SMTP_PASS,
+  },
+});
+
+export default transporter;
